619050522abee17dd65b9548ede8694eb6ad6177
mohammadraziei <mohammadraziei1375@gmail.com>
2019-02-27 00:42:44 +0330

    reinstruct script

--- Matlab/week2/q1_8.m
+++ Matlab/week2/q1_8.m
@@ -1,11 +1,16 @@
+%% init workspace
 clc;clear all;close all;
+addpath('../scripts');
+% includes :
+run handel_functions.m
+%% def problem parameters 
 t = linspace(-5,5,256);
 y = sinc(2*t);
-%%
+%% solution parameters
 N = 128;
 W = linspace(-2*pi,2*pi,N);
 Hs = freqz(y,1,W);
-%%
+%% plot A
 figure
 subplot(211);
 plot((W/pi) , abs(Hs));
@@ -13,7 +18,7 @@ xlabel('freq');
 subplot(212);
 plot(t,y);
 xlabel('time');
-%%
+%% plot B
 figure
 Hss = freqz(y(1:2:length(y)),1,W);
 subplot(211);
@@ -30,7 +35,7 @@ Ts = t(2)-t(1);
 hr =@(t_r,Ts,Sn) sinc( ( repmat(t_r + Ts - min(t_r),Sn,1) - repmat(((1:Sn)*Ts)' , 1 , length(t_r)) ) / Ts );
 t_re = linspace(-5,5,256*3);
 y_re = y * hr(t_re,Ts,256);
-%% plot 3 * fr
+%% plot 3 * fr || plot C
 figure
 subplot(211);
 plot(t,y,'o');hold on
