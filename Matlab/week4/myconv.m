e4c86b119e37425820b8d80ab078b7c384bf2de1
mohammadraziei <mohammadraziei1375@gmail.com>
2019-03-13 01:17:11 +0330

    remove (if) and (sum) in code

--- Matlab/week4/myconv.m
+++ Matlab/week4/myconv.m
@@ -1,14 +1,9 @@
 function y = myconv(h,x)
-    if length(h) > length(x)
-        t = x;
-        x = h;
-        h = t;
-    end
     L = length(h); M = length(x);
     y = zeros(1, M+L-1);
     xn = [zeros(1,L), x,zeros(1,L)];
     for n = 1:length(y)
         hn = [zeros(1,n),h(end:-1:1),zeros(1,M+L-n)];
-        y(n) = sum(hn .* xn);
+        y(n) = hn * xn';
     end
 end
