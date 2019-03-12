236b6fc80c2984c51e2ad5127c3a36ebac0ad19d
mohammadraziei <mohammadraziei1375@gmail.com>
2019-03-12 17:21:41 +0330

    fixed

--- Matlab/week4/myconv.m
+++ Matlab/week4/myconv.m
@@ -1,4 +1,9 @@
 function y = myconv(h,x)
+    if length(h) > length(x)
+        t = x;
+        x = h;
+        h = t;
+    end
     L = length(h); M = length(x);
     y = zeros(1, M+L-1);
     xn = [zeros(1,L), x,zeros(1,L)];
--- Matlab/week4/q2_1.m
+++ Matlab/week4/q2_1.m
@@ -1,7 +1,14 @@
 %% init workspace
 clc;clear all;close all;
-%% myconv
+%% a) test myconv
 x = ones(1,5);
 h = ones(1,3);
 y_my = myconv(x,h);
 y_mat = conv(x,h);
+
+%% b) use myconv
+h = 0.1*ones(1,10);
+t= 1:200;
+x = square(2*pi/50*t);
+% stem(x)
+
