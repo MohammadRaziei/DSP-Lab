4105c373ccb3eca366250f75398f1c2c23cdd613
mohammadraziei <mohammadraziei1375@gmail.com>
2019-04-16 17:10:09 +0430

    sound audio and filter created

---
+++ Matlab/week5/q2_3.m
@@ -0,0 +1,23 @@
+%% init workspace
+clc;clear all;close all;
+%% a)  audioread
+[aud,fs] = audioread('Audio01.wav');
+freqz(aud);
+plot(aud);
+%% b) filter
+load filter.mat
+% sound(aud,0.5*fs);
+
+n = 0:length(aud)-1;
+f0 = 1e4;
+s = 2*cos(2*pi*f0/fs*n);
+y0 = filter(Num,1,aud);
+y = filter(Num,1,s'.*y0);
+
+y0_new = filter(Num,1,y);
+y_new = filter(Num,1,s'.*y0_new);
+
+% sound(5*y_new,fs);
+
+% s = con(
+% conv(nu,m
